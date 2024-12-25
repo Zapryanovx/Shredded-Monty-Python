38e92d2a5af539f4f73b283eaf571ab4633cfc17
Ilian Zapryanov <68477629+Zapryanovx@users.noreply.github.com>
2024-12-26 00:05:27 +0200

    Update views.py

--- shredded_monty_python/main/views.py
+++ shredded_monty_python/main/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
-# Create your views here.
+def homepage(request):
+    return render(request, 'homepage.html')
+
+def register(request):
+    if request.method == "POST":
+        pass
+    return render(request, 'register.html')
+
+def login(request):
+    if request.method == "POST":
+        pass
+    return render(request, 'login.html')
