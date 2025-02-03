f48d09cf5279f3c1bc23045cc526aef09c70c598
Ilian Zapryanov <68477629+Zapryanovx@users.noreply.github.com>
2025-02-03 17:02:16 +0200

    Update settings.py

--- shredded_monty_python/shredded_monty_python/settings.py
+++ shredded_monty_python/shredded_monty_python/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -41,6 +42,7 @@ INSTALLED_APPS = [
     'main',
     'calculators',
     'user_profile',
+    'workouts',
 ]
 
 MIDDLEWARE = [
@@ -129,3 +131,6 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
