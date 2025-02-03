8c25d38743d6c56f2dd38feb12820c2f8a063835
Ilian Zapryanov <68477629+Zapryanovx@users.noreply.github.com>
2025-02-03 16:58:49 +0200

    Code Refactoring

--- shredded_monty_python/user_profile/views.py
+++ shredded_monty_python/user_profile/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
 
+
 @login_required
 def view_profile(request):
     return render(request, 'profile/view.html')
