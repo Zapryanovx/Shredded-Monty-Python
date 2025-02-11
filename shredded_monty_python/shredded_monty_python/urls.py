7c1e86a061b36d05fa73e0385b4f82195a725652
Ilian Zapryanov <68477629+Zapryanovx@users.noreply.github.com>
2025-02-11 02:27:35 +0200

    Update urls.py

--- shredded_monty_python/shredded_monty_python/urls.py
+++ shredded_monty_python/shredded_monty_python/urls.py
@@ -25,6 +25,7 @@ urlpatterns = [
     path('calculators/', include('calculators.urls')),
     path('profile/', include('user_profile.urls')),
     path('workouts/', include('workouts.urls')),
+    path('social/', include('social.urls')),
 ]
 
 if settings.DEBUG:
